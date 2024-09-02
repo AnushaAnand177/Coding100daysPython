76f4bcaa1850b288856c5aef9c26d70d3196853b
yellowgram1543 <anushaanand1543@gmail.com>
2024-09-02 16:37:56 +0530

    day 1 completed

---
+++ Coding 100 days/Day1.py
@@ -0,0 +1,6 @@
+print("Welcome guyzz!!")
+print("I will help today to generate a nam for your band\n "
+      "Answer the questions and you have a nice band name for a party")
+City = input("Where are you from?\n ")
+Pet = input("What is your pet name?\n ")
+print("Your band name could be " + City + Pet)
