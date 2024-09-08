c70c9a067677e131cdfaec7830a6fb22a6d1bcd8
yellowgram1543 <anushaanand1543@gmail.com>
2024-09-08 17:38:02 +0530

    Day 14 complete

--- Coding 100 days/Day 14/Day14_HigherLower_game.py
+++ Coding 100 days/Day 14/Day14_HigherLower_game.py
@@ -1,58 +1,71 @@
-from Day14_HigerLower_Art import logo, vs
 import random
-import Day14_HigherLower_data
+from Day14_HigherLower_data import data
+from Day14_HigerLower_Art import logo, vs
 
 print(logo)
 
 '''-------------------SELECTING DATA--------------------------'''
-def selecting():
-    data = random.choice(Day14_HigherLower_data.data)
-    for key, value in data.items():
-        return data["name"], data["description"], data["country"], data["follower_count"]
+
+
+def select_data():
+    person = random.choice(data)
+    return {
+        "name": person["name"],
+        "description": person["description"],
+        "country": person["country"],
+        "followers": person["follower_count"]
+    }
 
 
 '''------------------------INPUT FROM USER----------------------------'''
-def input_from_user():
-    return input("Who has more followers? Type 'A' or 'B': ")
+
+
+def get_user_choice():
+    return input("Who has more followers? Type 'A' or 'B': ").strip().upper()
+
+
+'''------------------------CHECK WINNER-----------------------------'''
+
+
+def is_correct_choice(choice, a_followers, b_followers):
+    if choice == "A":
+        return a_followers > b_followers
+    elif choice == "B":
+        return b_followers > a_followers
+    return False
 
 
 '''------------------------MAIN GAME-----------------------------------'''
+
+
 def game():
-    playing = True
-    name, description, country, follower_count = selecting()
-    name2, description2, country2 , follower_count2 = selecting()
     score = 0
+    playing = True
+
+    person_a = select_data()
+    person_b = select_data()
+
     while playing:
-        print(f"You're Current score: {score}.")
-        print(f"Compare A: {name}, a {description}, from {country}")
+        print(f"You're current score: {score}.")
+        print(
+            f"Compare A: {person_a['name']}, a {person_a['description']}, from {person_a['country']}")
         print(vs)
-        print(f"Against B: {name2}, a {description2}, from {country2}")
+        print(
+            f"Against B: {person_b['name']}, a {person_b['description']}, from {person_b['country']}")
 
-        choice = input_from_user()
-        if choice == "A":
-            if follower_count > follower_count2:
-                score += 1
-                name, description, country, follower_count = name2, description2, country2, follower_count2
-                name2, description2, country2, follower_count2 = selecting()
-                print("You are right!\n")
-            else:
-                print("Sorry, that's wrong.\n")
-                playing = False
-        elif choice == "B":
-            if follower_count2 > follower_count:
-                score += 1
-                name, description, country, follower_count = name2, description2, country2, follower_count2
-                name2, description2, country2, follower_count2 = selecting()
-                print("You are right!\n")
-            else:
-                print("Sorry, that's wrong.\n")
-                playing = False
+        choice = get_user_choice()
+        if is_correct_choice(choice, person_a['followers'], person_b['followers']):
+            score += 1
+            print("You are right!\n")
+            # Make 'B' the new 'A' and select a new 'B'
+            person_a = person_b
+            person_b = select_data()
         else:
-            print("Invalid input. Please enter 'A' or 'B'.\n")
+            print(f"Sorry, that's wrong. Final score: {score}.\n")
+            playing = False
 
 
-
-
-
+# Start the game
 game()
+
 
