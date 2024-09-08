4e0988a576a426eb267c8453f6485976c8d6ad2d
yellowgram1543 <anushaanand1543@gmail.com>
2024-09-08 10:16:03 +0530

    Day 12 complete

--- Coding 100 days/Day 12/Day12_NumberGuesser.py
+++ Coding 100 days/Day 12/Day12_NumberGuesser.py
@@ -3,10 +3,22 @@ import random
 print("Welcome to number guesser game")
 
 def random_number():
+    """
+    Generates a random number between 1 and 100.
+
+    Returns:
+        int: The randomly generated number.
+    """
     random_number = random.randint(1,100)
     return random_number
 
 def difficulty_level():
+    """
+    Asks the user to choose a difficulty level and returns the corresponding number of lives.
+
+    Returns:
+        int: The number of lives for the chosen difficulty level.
+    """
     level = input("Choose a difficulty. Type 'easy' or 'hard': ")
     if level == "easy":
         return 10
@@ -18,16 +30,36 @@ def difficulty_level():
 
 
 def life(lives):
+    """
+       Returns the number of lives for the current game.
+
+       Args:
+           lives (int): The number of lives for the current game.
+
+       Returns:
+           int: The number of lives for the current game.
+       """
     playing = True
     while playing:
         if lives != 0:
             return lives
 
 def guess(random_number, lives):
+    """
+        Plays the number guessing game.
+
+        Args:
+            random_number (int): The randomly generated number to guess.
+            lives (int): The number of lives for the current game.
+
+        Returns:
+            None
+        """
     no_of_lives = life(lives)
     print(f"You have {no_of_lives} lives left")
+    print("Guess a number between 1 and 100")
     while guess != random_number and no_of_lives != 0:
-        user_guess = int(input("Guess a number between 1 and 100: "))
+        user_guess = int(input("Make a guess: "))
         if user_guess == random_number:
             print("You guessed right")
             break
